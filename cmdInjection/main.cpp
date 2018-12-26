93f1fc08e9ed0db299d15d5c348159a7f917d401
Edson <edson@edson.portoalegre.pucrsnet.br>
2018-12-26 14:13:35 -0200

    argument passing v01

--- cmdInjection/main.cpp
+++ cmdInjection/main.cpp
@@ -1,33 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
-#include <unistd.h>
+#include <getopt.h>
 
 using namespace std;
 
-int main (int argc, char* argv[]){
-	int opt;
-	
-	cout << "Listing options" << endl;
-	opterr=0;
-	
-	while((opt=getopt(argc, argv, "abc"))!=-1){
-		
-		switch(opt){
-			case 'a':
-			  cout << "Opção " << char(optopt) << endl;			
-			  break;
-			case 'b':
-			  cout << "Opção " << char(optopt) << endl;			
-			  break;
-			case 'c':
-			  cout << "Opção " << char(optopt) << endl;			
-			  break;
-			case '?':
-			  cerr << "Opção desconhecida " << char(optopt) << endl;			
-			  break;
-			default:
-			  cerr << "Opção desconhecida ????"<< endl;						  			  
-		}
-		
-	}
+int main (int argc, char **argv)
+{
+  int c;
+
+  while (1){
+      static struct option long_options[] =
+        {
+          {"add",     no_argument,       0, 'a'}, // --add    ou -a
+          {"append",  no_argument,       0, 'b'}, // --append ou -b
+          {"delete",  required_argument, 0, 'd'}, // --delete ou -d
+          {"create",  required_argument, 0, 'c'}, // --create ou -c
+          {"file",    required_argument, 0, 'f'}, // --file   ou -f
+          {"help",    no_argument,       0, 'h'}, // --help   ou -h
+          {0, 0, 0, 0}
+        };
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
+
+      c = getopt_long (argc, argv, "habc:d:f:", long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+        case 0:
+          /* If this option set a flag, do nothing else now. */
+          if (long_options[option_index].flag != 0)
+            break;
+          printf ("option %s", long_options[option_index].name);
+          if (optarg)
+            printf (" with arg %s", optarg);
+          printf ("\n");
+          break;
+
+        case 'a':
+          puts ("option -a\n");
+          break;
+
+        case 'b':
+          puts ("option -b\n");
+          break;
+
+        case 'c':
+          printf ("option -c with value `%s'\n", optarg);
+          break;
+
+        case 'd':
+          printf ("option -d with value `%s'\n", optarg);
+          break;
+
+        case 'f':
+          printf ("option -f with value `%s'\n", optarg);
+          break;
+
+        case 'h':
+          printf ("option -h\n");
+          break;
+
+        case '?':
+          /* getopt_long already printed an error message. */
+          printf ("HEEEEELP \n");
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+/* Print any remaining command line arguments (not options). */
+  if (optind < argc)
+    {
+      printf ("non-option ARGV-elements: ");
+      while (optind < argc)
+        printf ("%s ", argv[optind++]);
+      putchar ('\n');
+    }
+
+  exit (0);
 }
